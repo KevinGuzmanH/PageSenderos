4a97812ceab4f5b6d3f4c2030945204a193aa5c4
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2022-03-07 23:54:26 -0500

    init

---
+++ src/main/java/com/keviny/paginasenderos/PaginaSenderosApplication.java
@@ -0,0 +1,13 @@
+package com.keviny.paginasenderos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaSenderosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginaSenderosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keviny/paginasenderos/PaginaSenderosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keviny.paginasenderos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaSenderosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
